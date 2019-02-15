b7e9964bea0ef392fb9c29020f61448c8be8dea1
lhqing <liuhq@ucsd.edu>
2019-02-15 15:32:21 +0800

    fix bug

--- allen/ccf.py
+++ allen/ccf.py
@@ -6,7 +6,6 @@ import allen
 import numpy as np
 
 PACKAGE_DIR = pathlib.Path(allen.__path__[0])
-print(PACKAGE_DIR)
 CEMBA_REGION = pd.read_table(PACKAGE_DIR / 'data/cemba_region_anno.tsv',
                              index_col=0)
 
@@ -44,15 +43,23 @@ def make_structure_mask_border(structure_id, ref_space):
     return mask
 
 
-def get_region_image(region, ref_space, tree):
-    region_coronal = 2100 + int(region[0]) * 600
-    mask = get_cemba_region_mask(region, ref_space, tree)
+def get_region_image(region, ref_space, tree, mask_only=False):
+    region_coronal = 2100 + int(region[:-1]) * 600
+    mask = get_cemba_region_3d_mask(region, ref_space, tree)
+    if mask_only:
+        return mask[int(region_coronal / ref_space.resolution[0]), :, :]
     coronal_img = ref_space.get_slice_image(0, region_coronal)
     img = mask[int(region_coronal / ref_space.resolution[0]), :, :][:, :, None] * coronal_img
     return img
 
 
-def get_cemba_region_mask(region_ids, ref_space, tree):
+def get_slice_image(region, ref_space):
+    region_coronal = 2100 + int(region[:-1]) * 600
+    coronal_img = ref_space.get_slice_image(0, region_coronal)
+    return coronal_img
+
+
+def get_cemba_region_3d_mask(region_ids, ref_space, tree):
     if isinstance(region_ids, str):
         region_ids = [region_ids]
     regions = ','.join(CEMBA_REGION.loc[region_ids]['Region'].tolist())
